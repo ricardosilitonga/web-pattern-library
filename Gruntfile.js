ce7a6da695ba7710aa5e9a01c22cdcdf750e48d4
Ricardo Silitonga <wellypina@gmail.com>
2017-09-20 16:39:20 +0700

    Menambahkan Grunt dengan plugin compass, uglify, dan watch

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+	// Project configuration
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		uglify : {
+			my_target : {
+				files : {
+					'_/js/script.js' : ['_/komponen/js/*.js']
+				} // files
+			} // my_target
+		}, // uglify
+		compass : {
+			dev : {
+				options : {
+					config : 'config.rb'
+				} // options
+			} // dev
+		}, // compass
+		watch : {
+			options: { livereload : true },
+			scripts : {
+				files : ['_/komponen/js/*.js'],
+				tasks : ['uglify']
+			}, // scripts
+			sass : {
+				files : ['_/komponen/sass/*.scss'],
+				tasks : ['compass:dev']
+			}, // sass
+			html : {
+				files : ['*.html']
+			} // html
+		} // watch
+	});
+
+	// Load the plugin that provides tasks.
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task(s).
+	grunt.registerTask('default', 'watch');
+}; //export
---
+++ config.rb
@@ -0,0 +1,4 @@
+css_dir = '_/css'
+sass_dir = '_/komponen/sass'
+javascripts_dir = '_/js'
+output_style = :nested
