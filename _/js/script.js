739688cc4d3db49dd2ab177130c21f604ed5701b
Ricardo Silitonga <wellypina@gmail.com>
2017-09-23 18:33:10 +0700

    Navigasi

--- _/js/script.js
+++ _/js/script.js
@@ -1,1 +1,1 @@
-function test(){console.log("hello")}
+$(function(){var e=$(".sf-menu");e.clone().removeClass().addClass("responsif-menu").prependTo(".kotak-navigasi nav"),e.superfish(),$("#tombol-responsif-menu").on("click",function(e){e.preventDefault(),$(".responsif-menu").slideToggle()})});
--- _/komponen/js/script.js
+++ _/komponen/js/script.js
@@ -1,3 +1,19 @@
-function test() {
-	console.log('hello');
-}
+$(function() {
+	/*
+	* Menu utama
+	*/
+	var menuUtama = $(".sf-menu");
+
+	// Membuat menu responsif dengan membuat kloning menuUtama.
+	menuUtama.clone().removeClass().addClass("responsif-menu").prependTo(".kotak-navigasi nav");
+
+	// Penerapan superfish pada menu.
+	menuUtama.superfish();
+
+	// Tombol responsif menu.
+	$("#tombol-responsif-menu").on("click", function(event) {
+		event.preventDefault();
+
+		$(".responsif-menu").slideToggle();
+	});
+});
